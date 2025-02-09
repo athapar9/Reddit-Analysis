6f0c2beedda6710e986770e64af1972bff7fff01
athapar9 <athapar@calpoly.edu>
2025-02-08 22:03:04 -0800

    pyspark progress

--- Week 5/week_5_analysis_duckdb.py
+++ Week 5/week_5_analysis_duckdb.py
@@ -7,8 +7,8 @@ def hex_to_rgb(hex_color):
     hex_color = hex_color.lstrip('#')
     return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
 
-def get_top_users(final_parquet, limit):
-    query_users = f"""
+def top_users(final_parquet, limit):
+    query = f"""
     SELECT user_id_hashed, COUNT(*) AS pixel_count
     FROM read_parquet('{final_parquet}')
     GROUP BY user_id_hashed
@@ -16,22 +16,22 @@ def get_top_users(final_parquet, limit):
     LIMIT {limit};
     """
     
-    df_users = duckdb.query(query_users).to_df()
+    df_users = duckdb.query(query).to_df()
 
     print(f"Retrieved {len(df_users)} users with the most pixels placed.")
     return df_users
 
-def get_user_pixel_positions(final_parquet, top_user_ids):
+def user_pixel_coords(final_parquet, top_user_ids):
     top_user_ids_str = ",".join(f"'{user_id}'" for user_id in top_user_ids)
     
-    query_positions = f"""
+    query = f"""
     SELECT x, y, user_id_hashed, pixel_color, timestamp
     FROM read_parquet('{final_parquet}')
     WHERE user_id_hashed IN ({top_user_ids_str})
     ORDER BY timestamp DESC
     """
 
-    df_positions = duckdb.query(query_positions).to_df()
+    df_positions = duckdb.query(query).to_df()
     
     print(f"Retrieved {len(df_positions)} pixel positions for top users.")
     return df_positions
@@ -45,12 +45,12 @@ def regenerate_image(final_parquet, pixel_x, pixel_y, radius=50):
     AND timestamp >= '2022-04-01 00:00:00' AND timestamp < '2022-04-04 24:00:00'
     """
     
-    pixel_data = duckdb.query(query).to_df()
+    pixels = duckdb.query(query).to_df()
 
     image_size = (2 * radius + 1, 2 * radius + 1)
     image = Image.new('RGB', image_size)
 
-    for _, row in pixel_data.iterrows():
+    for _, row in pixels.iterrows():
         x, y, hex_color = row['x'], row['y'], row['pixel_color']
         rgb_color = hex_to_rgb(hex_color)
         image.putpixel((x - (pixel_x - radius), y - (pixel_y - radius)), rgb_color)
@@ -71,12 +71,13 @@ def regenerate_canvas(df_positions, canvas_size):
 
 def main():
     final_parquet = "final.parquet"
-    df_top_users = get_top_users(final_parquet, limit=50)
-    top_user_ids = df_top_users['user_id_hashed'].tolist()
-    df_positions = get_user_pixel_positions(final_parquet, top_user_ids)
+    top_users_result = top_users(final_parquet, limit=10)
+    top_user_ids = top_users_result['user_id_hashed'].tolist()
+    coordinates_result = user_pixel_coords(final_parquet, top_user_ids)
+    print(coordinates_result.head())
     regenerate_image(final_parquet, 929, 1858)
     regenerate_image(final_parquet, 1647, 244)
-    regenerate_canvas(df_positions, canvas_size=(2000, 2000))  
+    regenerate_canvas(coordinates_result, canvas_size=(2000, 2000))  
 
 if __name__ == "__main__":
     main()
